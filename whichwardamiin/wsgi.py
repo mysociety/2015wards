7d1ad633b9b6c377411384f9b881de8e3f7e6d31
Steven Day <steve@mysociety.org>
2015-03-06 11:26:44 +0000

    Remove wsgimonitor from wsgi.py

--- whichwardamiin/wsgi.py
+++ whichwardamiin/wsgi.py
@@ -1,37 +1,12 @@
-"""
-WSGI config for whichwardamiin project.
+#!/usr/bin/env python
 
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
-
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
-"""
 import os
-import yaml
+import sys
 
-config_path = os.path.abspath( os.path.join( os.path.dirname(__file__), '..', 'conf', 'general.yml' ) )
-config = yaml.load(open(config_path))
+file_dir = os.path.realpath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.normpath(os.path.join(file_dir, '..')))
 
-if int(config.get('STAGING')):
-    import whichwardamiin.wsgi_monitor
-    whichwardamiin.wsgi_monitor.start(interval=1.0)
-    whichwardamiin.wsgi_monitor.track(config_path)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "whichwardamiin.settings")
-
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
