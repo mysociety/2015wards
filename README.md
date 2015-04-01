37ae9b2d842da0fe9447155d89500e9c059073ab
Zarino Zappia <mail@zarino.co.uk>
2015-04-01 12:28:39 +0100

    Completed Vagrantfile
    It requires some manual intervention, bit it’s a good start!

--- README.md
+++ README.md
@@ -5,6 +5,18 @@ A small project that uses [mapit](http://mapit.mysociety.org) to query ward
 boundaries and show users whether their local government ward will change in
 2015.
 
+Quick install: Vagrant
+----------------------
+
+This project has a Vagrantfile, but before you can run it, you need to make sure the following things have been set up:
+
+1. Copy `conf/general.yml-example` to `conf/general.yml`, set `DJANGO_SECRET_KEY` to some random string of characters, and set `2015WARDS_DB_USER`, `2015WARDS_DB_NAME`, and `2015WARDS_DB_PASS` all to `'mapit'`.
+2. Place a database dump from an existing MapIt Wards 2015 install, named `mapit_2015_ward_boundaries.sql.dump` into the root level of the project.
+
+Once these two requirements are satisfied, you can run `vagrant up`.
+
+If you run `vagrant up` without having created `conf/general.yml` and `mapit_2015_ward_boundaries.sql.dump`, it will warn you and exit, so you can create them.
+
 Install
 -------
 This is a fairly standard Django project, but because it uses Mapit, you need
--- scripts/provision.sh
+++ scripts/provision.sh
@@ -7,20 +7,41 @@ pwd
 now=$(date +"%T")
 echo "$now Running provision.sh"
 
+if [[ ! -f /vagrant/conf/general.yml ]] ; then
+    echo 'It looks like you forgot to create a general.yml file.'
+    echo 'Make sure you follow the two setup steps in README.md.'
+    exit
+fi
+
+if [[ ! -f /vagrant/mapit_2015_ward_boundaries.sql.dump ]] ; then
+    echo 'It looks like you forgot to prepare a MapIt Database dump.'
+    echo 'Make sure you follow the two setup steps in README.md.'
+    exit
+fi
+
 # Use the en_GB.utf8 locale
 sudo update-locale LANG=en_GB.utf8
 
 # Install the packages we need
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python-dev python-pip postgresql postgresql-9.1-postgis postgresql-server-dev-9.1 libgdal1-1.7.0 ruby-sass python-psycopg2 python-shapely python-yaml python-gdal
 
 cd /vagrant
 sudo pip install --timeout=120 --use-mirrors -r requirements.txt
 
-# Set up the Django database
-./manage.py syncdb --noinput
-./manage.py migrate
+# Set up PostgreSQL database
+sudo pg_dropcluster --stop 9.1 main
+sudo pg_createcluster --locale=en_GB.utf8 --start 9.1 main
+chmod +x scrips/postgis_template.sh
+sudo -u postgres scrips/postgis_template.sh
+echo "CREATE USER mapit WITH PASSWORD 'mapit' CREATEDB;" | sudo -u postgres psql
+sudo -u postgres createdb --owner=mapit mapit
+
+echo 'localhost:5432:mapit:mapit:mapit' > ~/.pgpass # Stops pg_restore asking for a password.
+chmod 600 ~/.pgpass
+
+# Import boundary data from existing MapIt database dump
+cat mapit_2015_ward_boundaries.sql.dump | pg_restore --clean --no-owner --username=mapit --dbname=mapit --host=localhost
 
 # Set shell login message
 echo "-------------------------------------------------
@@ -30,6 +51,6 @@ Run the web server with:
   cd /vagrant
   ./manage.py runserver 0.0.0.0:8000
 
-Then visit http://localhost:8000 to use 2015wards
+Then visit http://127.0.0.1:8000 to use 2015wards
 -------------------------------------------------
 " | sudo tee /etc/motd.tail > /dev/null
