ceaebfe47f344777a4a3aef3345beb708ce7aff6
Matthew Somerville <matthew-github@dracos.co.uk>
2017-03-07 13:34:08 +0000

    Fix context processer settings.

--- whichwardamiin/context_processors.py
+++ whichwardamiin/context_processors.py
@@ -3,10 +3,8 @@ from django.conf import settings
 def add_settings( request ):
     """Add some selected settings values to the context"""
     return {
-        'settings': {
-            'GOOGLE_ANALYTICS_ACCOUNT': settings.GOOGLE_ANALYTICS_ACCOUNT,
-            'DEBUG': settings.DEBUG,
-            'ELECTION_DATE': settings.ELECTION_DATE,
-            'ELECTION_YEAR': settings.ELECTION_YEAR,
-        }
+        'GOOGLE_ANALYTICS_ACCOUNT': settings.GOOGLE_ANALYTICS_ACCOUNT,
+        'DEBUG': settings.DEBUG,
+        'ELECTION_DATE': settings.ELECTION_DATE,
+        'ELECTION_YEAR': settings.ELECTION_YEAR,
     }
